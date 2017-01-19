265eac5f952db642ca59a4a01bd2c71e098308e3
ksindorf000 <ksindorf000@gmail.com>
2017-01-19 17:05:14 -0500

    Bare bones. Will not run.

---
+++ Cards.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class Cards
+    {
+        int value;
+        string name;
+
+        /************************************
+         * Constructor
+         ***********************************/
+        public Cards(int _value, string _name)
+        {
+            value = _value;
+            name = _name;
+        }
+
+        /************************************
+         * DisplayValue()
+         ***********************************/
+        public int DisplayValue()
+        {
+            return value;
+        }
+
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Deck
+    {
+        public static List<Cards> CreateBikeList()
+        {
+            var deck = new List<Cards>();
+            var rng = new Random();
+
+            for (int i = 0; i < 52; i++)
+            {
+                CreateCards(deck, rng);
+            }
+
+            return deck;
+        }
+
+        private static void CreateCards(List<Cards> deck, Random rng)
+        {
+            int value = rng.Next(1, 11);
+            string name;
+
+            deck.Add(new Cards(value, name));
+
+        }
+    }
+}
