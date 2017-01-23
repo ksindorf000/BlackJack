4a7027a0f82bc7db67a4bfea51fe5d8e79cefdea
ksindorf000 <ksindorf000@gmail.com>
2017-01-22 22:30:27 -0500

    Added "keepPlaying" loop

--- Program.cs
+++ Program.cs
@@ -9,31 +9,66 @@ namespace BlackJack
     class Program
     {
         static List<Player> playerList = new List<Player>();
+        static bool keepPlaying = true;
 
         static void Main(string[] args)
         {
-            //Create Deck
-            List<Cards> deck1 = Deck.CreateDeck();
+            string userInput;
+            bool valid = true;
 
-            //Create Player and Dealer
-            Dealer dealer = new Dealer();
-            Player player = new Player("Player");
+            while (keepPlaying)
+            {
+                //Create Deck
+                List<Cards> deck1 = Deck.CreateDeck();
 
-            //Add Player and Dealer to List<Players>
-            playerList.Add(dealer);
-            playerList.Add(player);
+                //Create Player and Dealer
+                Dealer dealer = new Dealer();
+                Player player = new Player("Player");
 
-            //Initial Deal of two cards per player
-            PlayGame.InitialDeal(playerList, deck1);
+                //Add Player and Dealer to List<Players>
+                playerList.Add(dealer);
+                playerList.Add(player);
 
-            //Players Hit or Stay
-            PlayGame.PlayerTurns(playerList, deck1);
+                //Initial Deal of two cards per player
+                PlayGame.InitialDeal(playerList, deck1);
 
-            //Dealer plays turn
-            Dealer.DealerTurn(dealer, deck1);
+                //Players Hit or Stay
+                PlayGame.PlayerTurns(playerList, deck1);
 
-            //Display Final
-            PlayGame.DisplayFinal(playerList);
+                //Dealer plays turn
+                Dealer.DealerTurn(dealer, deck1);
+
+                //Display Final
+                PlayGame.DisplayFinal(playerList);
+            }
+
+            while (valid)
+            {
+                Console.WriteLine("Would you like to play again? (Y/N): ");
+                userInput = Console.ReadLine().ToLower();
+                valid = ValidateInput(userInput);
+            }
+        }
+
+        private static bool ValidateInput(string userInput)
+        {
+            bool valid = true;
+
+            if (userInput == "y")
+            {
+                keepPlaying = true;
+            }
+            else if (userInput == "n")
+            {
+                keepPlaying = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                valid = false;
+            }
+
+            return valid;
         }
     }
 }
